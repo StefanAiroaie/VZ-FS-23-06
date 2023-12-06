ee207fe9c912204dffadd94fd209ddf53d0c2216
“StefanAiroaie” <airoaiestefan@gmail.com>
2023-12-06 14:27:11 +0100

    first 3 tasks done

---
+++ JavaScript-Intro/Intro-Level-1_2/assets/js/main.js
@@ -0,0 +1,10 @@
+// single line comment
+
+document.write("I have comments in my JavaScript code");
+// document.write("you can't see thise");
+
+document.write("I have multi line comments");
+
+/* document.write("you can't see thise");document.write("I have multi line comments");document.write("I have multi line comments");document.write("I have multi line comments");document.write("I have multi line comments");document.write("I have multi line comments");
+document.write("I have multi line comments");document.write("I have multi line comments"); document.write("I have multi line comments");document.write("I have multi line comments");document.write("I have multi line comments"); */
+
