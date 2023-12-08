78c3e3345d5365a35b8eca787eac1076103d32b7
“StefanAiroaie” <airoaiestefan@gmail.com>
2023-12-08 13:43:50 +0100

    done

---
+++ JavaScript/ClassList/Classlist-Level-1_1/assets/js/main.js
@@ -0,0 +1,16 @@
+/*
+Erstelle eine HTML-Datei mit der Headline “Meine Bucket-List” und einer Liste mit Dingen, die du gerne machen oder erleben möchtest.
+Gib der Headline und der Liste jeweils eine eindeutige ID.
+Erstelle im CSS zwei neue Klassen und vergebe dort eine font-family, sowie Textfarbe und Schriftgröße deiner Wahl. Natürlich darfst du auch gerne mehr Stylings verwenden.
+Du wirst eine Klasse für die Headline und eine Klasse für die Liste nutzen. Überleg dir also passende Namen und Stylings für die Klassen. Weise nun mit JavaScript der Headline und der Liste die jeweiligen Klassen zu.*/
+
+
+
+console.log("works?");
+
+const header = document.getElementById("bucketHeader")
+console.log(header);
+header.classList.add("css1")
+const list = document.getElementById("bucketList")
+console.log(list);
+list.classList.add("css2")
