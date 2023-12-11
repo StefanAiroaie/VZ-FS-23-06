6753ea6b01482c5bdef043de7f69265d7c50f009
“StefanAiroaie” <airoaiestefan@gmail.com>
2023-12-11 13:03:01 +0100

    Operators-Level-1_10-done

---
+++ JavaScript/Intro/Logical-Operators-Level-1_10/assets/js/main.js
@@ -0,0 +1,20 @@
+
+let x = 10
+let y = 15
+let z = 20
+
+let a = true
+let b = false
+
+console.log(x > y && x > y);
+console.log(x != y);
+console.log(z < y || z > y);
+console.log(x == z || x != y);
+console.log(x >= 10 && y <= 10);
+console.log(x * z < 100 || x * y > 100);
+
+console.log("2. teil");
+
+console.log(a && b);
+console.log(a || b);
+console.log(!a && b);
