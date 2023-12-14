1759f9c1eb65729dfde18a86e2f8e11b4a7a0d87
“StefanAiroaie” <airoaiestefan@gmail.com>
2023-12-14 11:54:30 +0100

    Functions-Vertiefung-Level-1_7-done

---
+++ JavaScript/Functions/Functions-Vertiefung-Level-1_7/assets/js/main.js
@@ -0,0 +1,29 @@
+/*
+
+
+DIn dieser Aufgabe soll geübt werden, wie Parameter eingesetzt werden können.
+Erstelle dazu eine Funktion, die einen Parameter hat und multipliziere diesen mit 2.
+Gib das Ergebnis mit return zurück und schaue dir das Ergebnis schließlich in der Konsole an.
+
+
+
+
+*/
+
+const calc = (num1) => num1 == 0 ? console.log("multiplizieren mit 0 geth nicht un blieb 0") : console.log(num1 * 2)
+
+// hero = (num1) => num1 = 0 ? return "multiplizieren mit 0 geth nicht") : reurn (num1 * 2)
+calc(0)
+calc(1)
+
+
+calc2(3)
+calc2(0)
+
+
+function calc2(num1) {
+    num1 == 0
+        ? console.log("multiplizieren mit 0 geth nicht")
+        : console.log(num1 * 2)
+
+}
