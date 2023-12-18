c05483e4cb220781fd49e7e80ad5acf0cca47c42
“StefanAiroaie” <airoaiestefan@gmail.com>
2023-12-18 13:21:27 +0100

    Array-Level-1_2-done

---
+++ JavaScript/Array/Array-Level-1_2/assets/js/main.js
@@ -0,0 +1,17 @@
+
+
+let person = ["Stefan", "Marian", "Fabian"]
+let friends = ["Sergiu", "Marius", "Marvin"]
+let favoriteFood = ["Pizza", "Pommes", "Döner"]
+
+
+console.log(person[0]);
+console.log(person[1]);
+console.log(person[2]);
+console.log(person[3]); // undefined weil sind nur 3 werte in Array und fang zu zahlen von 0
+console.log(friends[0]);
+console.log(friends[1]);
+console.log(friends[2]);
+console.log(favoriteFood[0]);
+console.log(favoriteFood[1]);
+console.log(favoriteFood[2]);
