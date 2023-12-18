faf50f731029ecb015371c8da9f24e0276683424
“StefanAiroaie” <airoaiestefan@gmail.com>
2023-12-18 13:02:07 +0100

    Array-Level-1_3-done

---
+++ JavaScript/Array/Array-Level-1_3/assets/js/main.js
@@ -0,0 +1,15 @@
+/*
+In dieser Übung lernst du, wie du Arrays in JavaScript definieren kannst.
+Erstelle 3 Arrays mit den Namen: person, friends und favoriteFood.
+In jedes dieser Arrays kommen 3 Werte. Bei person Angaben zu deiner Person, bei friends kommen deine Freund:innen rein und bei favoriteFood deine 3 Lieblingsgerichte.
+Lasse dir jedes Array in der Konsole ausgeben.*/
+
+
+let person = ["Stefan", "Marian", "Fabian"]
+let friends = ["Sergiu", "Marius", "Marvin"]
+let favoriteFood = ["Pizza", "Pommes", "Döner"]
+
+
+console.log(person.length);
+console.log(friends.length);
+console.log(favoriteFood.length);
