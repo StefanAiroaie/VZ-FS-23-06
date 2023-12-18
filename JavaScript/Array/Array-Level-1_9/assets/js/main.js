5e511c9ece7264d4ba595139fab1369778762b8d
“StefanAiroaie” <airoaiestefan@gmail.com>
2023-12-18 14:16:13 +0100

    Array-Level-1_9-done

---
+++ JavaScript/Array/Array-Level-1_9/assets/js/main.js
@@ -0,0 +1,14 @@
+
+
+let NumberArr = [23, 54, 75]
+console.log(NumberArr);
+NumberArr.push(1, 2, 3, 4, 5,)
+console.log(NumberArr);
+NumberArr.unshift(5, 4, 3, 2, 1)
+console.log(NumberArr);
+NumberArr.pop()
+NumberArr.pop()
+console.log(NumberArr);
+NumberArr.shift()
+NumberArr.shift()
+console.log(NumberArr);
