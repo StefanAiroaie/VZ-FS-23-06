c89e66ba14cd14497ea17839cbdecb64c4e93eef
“StefanAiroaie” <airoaiestefan@gmail.com>
2023-12-18 13:34:32 +0100

    Array-Level-1_5-done

---
+++ JavaScript/Array/Array-Level-1_5/assets/js/main.js
@@ -0,0 +1,32 @@
+/*
+
+Lernziel: Verstehen und Anwenden der Array-Methode push().
+Erstelle ein Array und ergänze es mit der Array-Methode push(). Mit push() kannst du einen oder mehrere Werte gleichzeitig an das Ende eines Arrays hinzufügen.
+Verwende push(), um mindestens 2 neue Werte zu deinem Array hinzuzufügen.
+Verwende console.log(), um den Inhalt und die Länge deines Arrays vor und nach dem Hinzufügen neuer Werte anzuzeigen. Beobachte die Veränderungen in der Konsole.
+*/
+
+
+let person = ["Stefan", "Marian", "Fabian"]
+let friends = ["Sergiu", "Marius", "Marvin"]
+let favoriteFood = ["Pizza", "Pommes", "Döner"]
+
+
+person.push("Marius", "Adam");
+console.log(person);
+console.log(person.pop());
+console.log(person);
+
+
+
+friends.push("George", "Maria")
+console.log(friends);
+console.log(friends.pop());
+console.log(friends);
+
+
+
+favoriteFood.push("Pasta", "Avoscado")
+console.log(favoriteFood);
+console.log(favoriteFood.pop());
+console.log(favoriteFood);
