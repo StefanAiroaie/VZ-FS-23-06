341ef5cd6c4fd398869cc8a8673f42b24401ef86
“StefanAiroaie” <airoaiestefan@gmail.com>
2023-12-18 13:47:40 +0100

    Array-Level-1_6-done

---
+++ JavaScript/Array/Array-Level-1_6/assets/js/main.js
@@ -0,0 +1,11 @@
+
+
+let person = ["Stefan", "Marian", "Fabian"]
+
+
+person.push("Marius", "Adam");
+console.log(person);
+
+let newPerson = person.shift()
+console.log(newPerson);
+console.log(person);
