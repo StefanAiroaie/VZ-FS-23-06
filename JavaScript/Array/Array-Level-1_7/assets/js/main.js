5b62349167b92e87aa8aa77e2624c9af2ddb802a
“StefanAiroaie” <airoaiestefan@gmail.com>
2023-12-18 13:52:17 +0100

    Array-Level-1_7-done

---
+++ JavaScript/Array/Array-Level-1_7/assets/js/main.js
@@ -0,0 +1,17 @@
+
+
+let person = ["Stefan", "Marian", "Fabian"]
+let friends = ["Sergiu", "Marius", "Marvin"]
+let favoriteFood = ["Pizza", "Pommes", "Döner"]
+
+
+
+console.log(friends);
+console.log(friends.unshift("Ion", "Vasile"));
+console.log(friends);
+
+
+
+console.log(favoriteFood);
+favoriteFood.unshift("Pasta", "Burger")
+console.log(favoriteFood);
