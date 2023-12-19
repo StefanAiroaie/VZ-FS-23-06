1b5b0373faec1b7dba590ab3b0718cfea8b34350
“StefanAiroaie” <airoaiestefan@gmail.com>
2023-12-19 23:43:37 +0100

    Array-Sort-Level-2_1-done

--- JavaScript/Array/Array-Sort-Level-2_1/assets/js/main.js
+++ JavaScript/Array/Array-Sort-Level-2_1/assets/js/main.js
@@ -1,24 +1,14 @@
-/*
-- Schreibe ein kleines Programm, das Zeichenketten umkehrt.
-- Diese Funktion soll jeden String, der ihr übergeben wird umdrehen können (nutze einen Parameter).
-- Du wirst Array-Methoden verwenden müssen, um den String umzudrehen.
-- Rufe die Funktion mit deinem Namen als Parameter auf.
-- Überprüfe das Ergebnis in der Konsole.
-- Versuche diese Begriffe umzukehren: Sergio, Hannah, Regallager, Reliefpfeiler und Rentner.
-- Versuche diese Sätze umzukehren: "Ella mag alle Bohnen" und "han nesaH has ennaH".
-
-- Wenn es mehrere Wörter gibt: Drehe jedes Wort einzeln um.
-- Tipp: Du musst schauen, ob es in der übergebenen Zeichenkette mehrere Wörter gibt!
-*/
-
-
-
 let namen = ["Sergio", "Hannah", "Regallager", "Reliefpfeiler", "Rentner"]
-let text = ["Ella mag alle Bohnen", "han nesaH has ennaH"]
+console.log(namen);
 
-function drehen(parameter) {
-    parameter.reverse()
-    console.log(parameter);
+const reverse = (word) => {
+    let reversedWord = word.map(cuvant => cuvant.split("").reverse().join(""))
+    console.log(reversedWord);
 }
 
-drehen(namen)
+reverse(namen)
+
+
+let text = ["Ella mag alle Bohnen", "han nesaH has ennaH"]
+
+reverse(text)
