84c37178f2626162beddc39a07acffcd9ce45379
“StefanAiroaie” <airoaiestefan@gmail.com>
2023-12-19 12:44:52 +0100

    Array-Level-1_8-done

---
+++ JavaScript/Array/Array-Level-1_8/assets/js/main.js
@@ -0,0 +1,12 @@
+
+
+let lieblingsreiseziele = ["USA", "DE", "IT", "FR", "NL"]
+console.log(lieblingsreiseziele);
+let ziele2023 = lieblingsreiseziele.slice(0, 2)
+console.log(ziele2023);
+
+let ziele2024 = lieblingsreiseziele.slice(3, 5)
+console.log(ziele2024);
+
+
+
