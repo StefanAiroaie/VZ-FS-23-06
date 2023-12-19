48d5f700c383630d9fc8647b9b3da860aa0d54e4
“StefanAiroaie” <airoaiestefan@gmail.com>
2023-12-19 13:59:28 +0100

    Array-Level-1_10-done

---
+++ JavaScript/Array/Array-Level-1_10/assets/js/main.js
@@ -0,0 +1,15 @@
+
+const text = "It's okay, Bender. I like cooking too. Oh, you're a dollar naughtier than most. My fellow Earthicans, as I have explained in my book 'Earth in the Balance', and the much more popular 'Harry Potter and the Balance of Earth', we need to defend our planet against pollution. Also dark wizards."
+
+console.log(text);
+console.log(text.length);
+
+let textSplitUntilPunct = text.split(".")
+console.log(textSplitUntilPunct);
+
+let textSplitUntilLeer = text.split(" ")
+console.log(textSplitUntilLeer);
+
+
+let textSplitJedeLetter = text.split("")
+console.log(textSplitJedeLetter);
