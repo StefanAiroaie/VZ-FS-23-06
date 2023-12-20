4e1f661149734bf1d82d6beb8fb8038e2c964122
“StefanAiroaie” <airoaiestefan@gmail.com>
2023-12-20 15:25:52 +0100

    Loops-Level-2_2-a part done

---
+++ JavaScript/Loops/Loops-Level-2_2/assets/js/main.js
@@ -0,0 +1,12 @@
+
+
+result = document.querySelector("#result")
+function loop() {
+    let inputNumber = document.querySelector("#inputNumber").value
+    for (let i = 1; i <= inputNumber; i++) {
+        result.innerHTML += "o"
+        
+    }
+
+
+}
