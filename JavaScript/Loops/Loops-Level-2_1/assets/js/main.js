3c256b1f48c17823cc8622b4ba64e690ac87870e
“StefanAiroaie” <airoaiestefan@gmail.com>
2023-12-20 14:11:39 +0100

    Loops-Level-2_1-done

---
+++ JavaScript/Loops/Loops-Level-2_1/assets/js/main.js
@@ -0,0 +1,18 @@
+
+function imageArray() {
+    let returnArray = []
+    for (i = 1; i <= 100; i++)
+        if (i <= 9) {
+            returnArray.push("image_00" + i + ".jpg")
+
+        }
+        else if (i <= 99) {
+            returnArray.push("image_0" + i + ".jpg")
+        }
+        else {
+            returnArray.push("image_" + i + ".jpg")
+        }
+    console.log(returnArray);
+}
+
+imageArray()
