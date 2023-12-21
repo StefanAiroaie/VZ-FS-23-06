40e4d3fafb5cfc38ccb7c105500fd2018737a37c
“StefanAiroaie” <airoaiestefan@gmail.com>
2023-12-21 15:45:24 +0100

    Dom-Elements-Level-1_1-bonus hinzugefügt

--- JavaScript/DOM/Dom-Elements-Level-1_1/assets/js/main.js
+++ JavaScript/DOM/Dom-Elements-Level-1_1/assets/js/main.js
@@ -6,6 +6,10 @@ let inputText = document.querySelector("#inputText")
 let myList = document.querySelector("#myList")
 
 inputText.addEventListener("change", () => {
+    if (inputText.value === "") {
+        alert("Ohne text funktioniert nicht")
+        return
+    }
     let list = document.createElement("li")
     list.textContent = inputText.value
     myList.appendChild(list)
