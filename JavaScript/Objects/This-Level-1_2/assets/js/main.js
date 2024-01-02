165884a1735efa9fce3037af649e747ab612cc3e
“StefanAiroaie” <airoaiestefan@gmail.com>
2024-01-02 16:33:02 +0100

    This-Level-1_2-done

---
+++ JavaScript/Objects/This-Level-1_2/assets/js/main.js
@@ -0,0 +1,15 @@
+let person = {
+    vorname: 'Anton',
+    nachname: 'Fish',
+    alter: 34,
+    familienstand: 'ledig',
+    groesse: '178cm',
+    profil: function () {
+        return `<p> Name: ${this.vorname} ${this.nachname}</p> 
+        <p>Alter: ${this.alter}</p> 
+        <p>Körpergroße: ${this.groesse}</p> 
+        <p>Familienstand: ${this.familienstand}</p> `
+    }
+};
+//gib das Profil von Anton mit diesem Code im Element mit der "person" aus.
+document.getElementById("person").innerHTML = person.profil();
