88965f34cb5a6c8354a464440e890f5f5a74915e
“StefanAiroaie” <airoaiestefan@gmail.com>
2024-01-02 14:31:03 +0100

    Objekte-Level-1_7-done

---
+++ JavaScript/Objects/Objekte-Level-1_7/assets/js/main.js
@@ -0,0 +1,50 @@
+let myMusic = [
+    {
+        artist: "The Beatles",
+        title: "Abbey Road",
+        release_year: 1969,
+        medium: ["LP", "CD", "MC", "Download"],
+        gold: true
+    },
+    {
+        artist: "Pink Floyd",
+        title: "Dark Side of the Moon",
+        release_year: 1978,
+        medium: ["CS", "CD", "LP", "Download"],
+        gold: true
+    },
+    {
+        artist: "Led Zeppelin",
+        title: "Led Zeppelin IV",
+        release_year: 1971,
+        medium: ["CS", "LP", "Download"],
+        gold: true
+    },
+    {
+        artist: "Metallica",
+        title: "Kill ’Em All und Ride the Lightning",
+        release_year: 1983,
+        medium: ["LP", "CD", "MC", "Download"],
+        gold: true
+    }
+];
+
+
+myMusic.forEach((infos) => {
+    const createDiv = document.createElement("div")
+
+    const title = document.createElement("p")
+    title.innerHTML = infos.title
+    createDiv.appendChild(title)
+
+    const artist = document.createElement("p")
+    artist.textContent = infos.artist
+    createDiv.appendChild(artist)
+
+    const medium = document.createElement("p")
+    medium.textContent = infos.medium
+    createDiv.appendChild(medium)
+
+    document.body.appendChild(createDiv)
+})
+
