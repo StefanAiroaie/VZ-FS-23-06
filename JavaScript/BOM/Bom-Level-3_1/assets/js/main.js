4027fed3ebd94d3e434661c9ce07b051d5e73641
“StefanAiroaie” <airoaiestefan@gmail.com>
2024-01-04 13:41:59 +0100

    problems

--- JavaScript/BOM/Bom-Level-3_1/assets/js/main.js
+++ JavaScript/BOM/Bom-Level-3_1/assets/js/main.js
@@ -1,17 +1,11 @@
 
 
 
-
-// catWalk()
-// pause()
-// turn()
-// catSpeed()
-
-
 let maxwidth = window.innerWidth
 let cat = document.querySelector("#cat")
 let catstart = 0
-let catend = 1200
+let catEnd = maxwidth - 304
+console.log(catEnd);
 // let catend = maxwidth
 
 
@@ -20,35 +14,22 @@ let catWalk = () => {
         catstart += 5
         cat.style.left = `${catstart}px`
         console.log("aici e in functie", catstart);
-        if (catstart == 1200) {
-            catstart += 5
-            cat.style.top = `${catstart}px`
-            console.log(catstart);
-            console.log("aici e in functie", catstart);
+        if (catstart == catEnd) {
+            turn()
+            clearInterval(trainSpeed)
         }
     }
-    trainSpeed = setInterval(animateCat, 200)
 
-    console.log(catend);
+    trainSpeed = setInterval(animateCat, 10)
 
 }
+
+
+let turn = () => cat.style.transform += "scaleX(-1)"
+
 
 let pause = () => clearInterval(trainSpeed)
 console.log("aici", catstart);
 
 console.log(maxwidth);
 
-
-
-// function startTrain() {
-//     let animationTrain = () => {
-//         leftPos += 10
-//         train.style.left = `${leftPos}px`
-//     }
-//     trainInterval = setInterval(animationTrain, 100)
-
-// }
-
-// function stopTrain() {
-//     clearInterval(trainInterval)
-// }
