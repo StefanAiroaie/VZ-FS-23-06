65f7caa80bad894ae0d60282496a569b25ed329f
“StefanAiroaie” <airoaiestefan@gmail.com>
2024-02-20 18:11:37 +0100

    updates

--- Backend/Import-Export/Import:Export-Level-1_2/functions.js
+++ Backend/Import-Export/Import:Export-Level-1_2/functions.js
@@ -1,8 +1,16 @@
 
 // sort function
 const sortFunc = (array) => {
-    return array.sort()
+    return array.sort((a, b) => {
+        if (a < b) {
+            return -1
+        } else if (a > b) {
+            return 1
+        }
+        return 0
+    })
 }
+
 
 const firstEllement = (array) => {
     return array[0]
@@ -22,4 +30,25 @@ const allMinusFirst = (array) => {
 }
 
 
-export default { sortFunc, firstEllement, allMinusLast, allMinusFirst, onlyLast }
+const sumFunc = (array) => {
+    let result = 0
+    for (let i = 0; i < array.length; i++) {
+        result += array[i]
+    }
+    return result
+
+}
+
+
+const FirstLetterToUppercase = (string) => {
+    return chartAt[0].toUpperCase()
+
+
+}
+
+const stringToUppercase = (array) => {
+    return array.map(el => el.toUpperCase())
+}
+
+
+export default { sortFunc, firstEllement, allMinusLast, allMinusFirst, onlyLast, sumFunc, stringToUppercase }
--- Backend/Import-Export/Import:Export-Level-1_2/index.js
+++ Backend/Import-Export/Import:Export-Level-1_2/index.js
@@ -9,3 +9,11 @@ console.log("functions.firstEllement(names)", functions.firstEllement(names));
 console.log("functions.allMinusLast(names)", functions.allMinusLast(names));
 console.log("functions.onlyLast(names)", functions.onlyLast(names));
 console.log("functions.allMinusFirst(names)", functions.allMinusFirst(names));
+
+
+console.log("functions.sumFunc(numbers)", functions.sumFunc(numbers));
+
+console.log("functions.toUpCasefunc(names)", functions.stringToUppercase(names));
+
+
+console.log(FirstLetterToUppercase("magic"));
