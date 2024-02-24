043b12206a0ccacf75fa0be2361752834ad278f4
“StefanAiroaie” <airoaiestefan@gmail.com>
2024-02-24 19:31:37 +0100

    promise level1_1 update time out

--- Backend/Promises/Level-1_1/index.js
+++ Backend/Promises/Level-1_1/index.js
@@ -2,24 +2,20 @@
 
 const duplicateNumber = (numberToBeUsed) => {
     return new Promise((resolve, reject) => {
-        if (typeof numberToBeUsed === "number")
-            resolve(numberToBeUsed * 2)
-        else {
-            reject("hei asta nu e numar")
-        }
-
+        //set timeout
+        setTimeout(() => {
+            // prufe ob number ist
+            if (typeof numberToBeUsed === "number") {
+                resolve(numberToBeUsed * 2)
+            }
+            else {
+                reject("hei asta nu e numar")
+            }
+        }, 1000)
     })
 }
 
 console.log("hier ist funktion direct gerufen => Result:", duplicateNumber(5));
-
-
-duplicateNumber("5f")
-    .then((result) => {
-        console.log("hier ist funktion mit then => Result:", result);
-    }).catch((reject) => {
-        console.log("hei hier ist kein nummer", reject);
-    });
 
 duplicateNumber(5)
     .then((result) => {
@@ -28,6 +24,12 @@ duplicateNumber(5)
         console.log("hei hier ist kein nummer", reject);
     });
 
+duplicateNumber("5f")
+    .then((result) => {
+        console.log("hier ist funktion mit then => Result:", result);
+    }).catch((reject) => {
+        console.log("hei hier ist kein nummer", reject);
+    });
 
 
 
